c54338b33695869a701dd94d9ebceda0d1180bbf
KuanYu <george310909@gmail.com>
2023-10-21 23:34:17 +0800

    bug fixed

--- src/views/home/Field.svelte
+++ src/views/home/Field.svelte
@@ -4,7 +4,7 @@
 	export let value: string = "";
 </script>
 
-<span class="d-flex flex-column">
+<span class="d-flex flex-column w-100 pe-2">
 	<div>{title}</div>
 	<div class="mt-2 d-flex flex-row justify-content-start align-items-center flex-fill">
 		<input class="rounded col-12" type="text" {placeholder} bind:value />
--- src/views/home/Home.svelte
+++ src/views/home/Home.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="d-flex flex-column">
-	<h4>MEMBER CENTER | 會員資訊</h4>
-	<div class="mt-3 p-3 bg-black text-white">基本資料</div>
-	<div class="w-100 p-3">
+	<h4 class="px-3">MEMBER CENTER | 會員資訊</h4>
+	<div class="mt-3 p-3 mx-3 mx-sm-0 bg-black text-white">基本資料</div>
+	<div class="w-100 px-3">
 		<div class="mt-2 d-flex flex-column flex-sm-row justify-content-start align-items-center w-100">
 			<span class="d-flex flex-row justify-content-start align-items-center flex-sm-fill col-12 col-sm-auto">
 				<Field title="姓名" placeholder="姓名" />
@@ -32,7 +32,7 @@
 		</div>
 		<div class="d-flex flex-column w-100">
 			<div class="mt-2 w-100 d-none d-sm-flex flex-row justify-content-start align-items-center">地址</div>
-			<div class="p-3 w-100 d-flex d-sm-none flex-row justify-content-start align-items-center bg-black text-white">地址</div>
+			<div class="mt-2 p-3 w-100 d-flex d-sm-none flex-row justify-content-start align-items-center bg-black text-white">地址</div>
 			<div class="mt-2 d-flex flex-column flex-sm-row justify-content-start align-items-start align-sm-items-center">
 				<div class="d-flex flex-row justify-content-start align-items-center flex-fill me-2">
 					<input class="rounded w-100" type="text" placeholder="城市/縣" />
@@ -41,8 +41,8 @@
 					<input class="rounded w-100" type="text" placeholder="地區" />
 				</div>
 			</div>
-			<div class="mt-2 d-flex flex-row justify-content-start align-items-center flex-fill">
-				<input class="rounded" type="text" placeholder="收件人地址" />
+			<div class="mt-2 d-flex flex-row justify-content-start align-items-center col-12 col-sm-6">
+				<input class="rounded w-100" type="text" placeholder="收件人地址" />
 			</div>
 		</div>
 	</div>
